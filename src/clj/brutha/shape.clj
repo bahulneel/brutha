5c475974e4c2c0496469e58896eafcb8eb64f305
Neel Upadhyaya <bahulneel@gmail.com>
2015-02-22 20:19:01 +0000

    Ref shapes and transformations

--- src/clj/brutha/shape.clj
+++ src/clj/brutha/shape.clj
@@ -1,8 +1,10 @@
-(ns brutha.shape)
+(ns brutha.shape
+  (:require [brutha.geo :as geo]))
 
 (defprotocol IShape
-  (-position [_])
-  (-hash [_]))
+  (-position [s])
+  (-id [s])
+  (-scale [s]))
 
 (defn shape? [s]
   (satisfies? IShape s))
@@ -10,5 +12,41 @@
 (defn position [s]
   (-position s))
 
-(defn shash [s]
-  (-hash s))
+(defn id [s]
+  (-id s))
+
+(defn scale [s]
+  (-scale s))
+
+(defrecord RefShape [id p s]
+  IShape
+  (-id [_]
+    id)
+  (-position [_]
+    p)
+  (-scale [_]
+    s))
+
+(defn ref-shape
+  ([id]
+     (ref-shape id [0 0] [1 1]))
+  ([id p s]
+     (->RefShape id p s)))
+
+(defrecord TransformedShape [shape p s]
+  IShape
+  (-id [_]
+    (-id shape))
+  (-position [_]
+    (geo/p+ p (-position shape)))
+  (-scale [_]
+    (geo/dot s (-scale shape))))
+
+(defn x-shape [shape p s]
+  (->TransformedShape shape p s))
+
+(defn s+ [shape v]
+  (x-shape shape v [1 1]))
+
+(defn s* [shape s]
+  (x-shape shape [0 0] s))
